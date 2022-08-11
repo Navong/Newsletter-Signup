ebb57560d985362f0e4adb186e59941a8b5d8882
jumin <xcvzxc>
2022-08-11 12:47:30 +0900

    add api

--- app.js
+++ app.js
@@ -19,7 +19,36 @@ app.post('/', (req, res)=>{
     var lastName = req.body.lname;
     var email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members:[
+            {
+                email_address : email,
+                status: 'subscribed',
+                merge_fields :{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    //convert to flat form for send to api server
+
+    const url = 'https://us9.api.mailchimp.com/3.0/lists/ca75e8e5bc';
+    const options = {
+        method: 'POST',
+        auth: 'navong1:84497754eaf8b9f3d24591fa87b61b8d-us9'
+    };
+
+    const request = https.request(url , options, (response)=>{
+        response.on('data', (data)=>{
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 //2. set up server port
